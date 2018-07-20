4788e9f8e772e7409b32370667cb6dde1febe611
Shotaro Higashi <shotaro@east-higashi.net>
2018-07-20 19:45:50 +0900

    update template files

--- attend_manage/console/urls.py
+++ attend_manage/console/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('status_all/', views.status_all, name='status_all'),
+    path('schedule/', views.schedule, name='schedule'),
 ]
--- attend_manage/console/views.py
+++ attend_manage/console/views.py
@@ -4,7 +4,24 @@ from django.http import HttpResponse
 
 def index(request):
     params = {
+        'title':'ホーム',
         'name_jp':'東　昭太朗',
-        'status' : True,
+        'status':True,
+        }
+    return render(request, 'console/index.html', params)
+
+def status_all(request):
+    params = {
+        'title':'研究室の今',
+        'name_jp':'東　昭太朗',
+        'status' : 0
+        }
+    return render(request, 'console/index.html', params)
+
+def schedule(request):
+    params = {
+        'title':'本日の予定',
+        'name_jp':'東　昭太朗',
+        'status' : 1
         }
     return render(request, 'console/index.html', params)
