7c267755a72ab80eae640b5e1b66e8551d9d63df
Shotaro Higashi <shotaro@east-higashi.net>
2018-08-02 15:47:01 +0900

    define model

--- attend_manage/console/models.py
+++ attend_manage/console/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class UserDetail(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # 参照先のレコードが削除されるとこちらも連動して削除される
+    name_ja = models.CharField(max_length=50)
+
+class AttendanceLog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT) # このテーブル上のレコードが削除されても参照先のテーブルからユーザ情報を削除しない
+    time_in = models.DateTimeField()
+    time_out = models.DateTimeField()
+
+class InRoom(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    available = models.BooleanField()
