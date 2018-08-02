6a6a65cbadfd8bc2bcf80999b6771b67a31ddaf1
Shotaro Higashi <shotaro@east-higashi.net>
2018-08-02 15:48:00 +0900

    update urls.py

--- attend_manage/console/urls.py
+++ attend_manage/console/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('status_change/', views.status_change, name='status_change'), # for ajax in index.html
     path('status_all/', views.status_all, name='status_all'),
-    path('schedule/', views.schedule, name='schedule'),
+    #path('schedule/', views.schedule, name='schedule'),
 ]
