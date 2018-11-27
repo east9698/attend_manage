9aa6979c1bafc1c05be34cd81a02e139543b1444
Shotaro Higashi <shotaro@east-higashi.net>
2018-11-27 20:01:25 +0900

    add some missing library in authentication.models

--- authentication/models.py
+++ authentication/models.py
@@ -4,8 +4,8 @@ from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
-from .validators import UnicodeUsernameValidator
-
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.contrib.auth.models import PermissionsMixin
 
 class UserManager(BaseUserManager):
     use_in_migrations = True
